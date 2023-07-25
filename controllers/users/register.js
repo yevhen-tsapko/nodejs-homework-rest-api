da25e1461d0cd2a4ab4379c323e4d65529b8bb1b
Yevhen Tsapko <evgentsa@gmail.com>
2023-07-25 10:07:00 +0300

    Update register.js

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -3,15 +3,20 @@ const bcrypt = require("bcrypt");
 const gravatar = require("gravatar");
 
 const register = async (req, res) => {
-  const { email, name, password } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user !== null) {
     res.status(409).json({ message: "Email in use" });
   }
   const avatarURL = gravatar.url(email, { s: "100", r: "x", d: "retro" });
   const passwordHash = await bcrypt.hash(password, 10);
-  await User.create({ ...req.body, password: passwordHash, avatarURL });
-  res.status(201).json({ user: { name, email } });
+  const newUser = await User.create({
+    email,
+    password: passwordHash,
+    avatarURL,
+  });
+  const { subscription } = newUser;
+  res.status(201).json({ user: { subscription, email } });
 };
 
 module.exports = register;
